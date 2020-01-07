fcf17f792a6cf84be9dc4a7f15dae771be85a455
NewIncome <alfredo_cardenas@live.com>
2020-01-07 13:08:29 -0600

    Added 2nd Excercises and Quiz.

--- 01-SELECT_basics.sql
+++ 01-SELECT_basics.sql
@@ -1,3 +1,12 @@
+/*name		continent	area 		population	gdp
+Afghanistan	Asia		652230		25500100	20343000000
+Albania		Europe		28748 		2831741 	12960000000
+Algeria		Africa		2381741 	37100000 	188681000000
+Andorra		Europe		468			78115	 	3712000000
+Angola		Africa		1246700 	20609294 	100990000000
+....
+*/
+
 /* 1.- Introducing the world table of countries
 The example uses a WHERE clause to show the population of 'France'.
 Note that strings (pieces of text that are data) should be in 'single quotes';
